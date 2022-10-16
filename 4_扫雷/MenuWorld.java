c08eff890d58123bea22490c4c165b67303ee8eb
liuyx <1517482303@qq.com>
2022-10-16 11:22:30 +0800

    sweep mine 2.1

--- 4_扫雷/MenuWorld.java
+++ 4_扫雷/MenuWorld.java
@@ -16,6 +16,9 @@ public class MenuWorld extends World
     public MenuWorld() {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        addObject(new Actor() {{
+            setImage(new GreenfootImage("title.png"));
+        }}, getWidth() / 2, getHeight() / 2 - 140);
         addObject(new Diff("easy"), getWidth() / 2 - 185, getHeight() / 2);
         addObject(new Diff("medium"), getWidth() / 2, getHeight() / 2);
         addObject(new Diff("hard"), getWidth() / 2 + 185, getHeight() / 2);
